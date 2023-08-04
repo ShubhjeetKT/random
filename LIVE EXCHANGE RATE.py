cc2e02d540eec5809bc7d4d2ad930b8833a08092
SHUBHJEET KUMAR TIWARI <shubhjeet007@gmail.com>
2023-08-04 21:47:28 +0530

    Create LIVE EXCHANGE RATE.py

---
+++ LIVE EXCHANGE RATE.py
@@ -0,0 +1,13 @@
+import urllib3
+import json
+url = "https://api.exchangerate.host/latest"
+http = urllib3.PoolManager()
+response =  http.request('GET',url)
+data = response.data
+currency = json.loads(data)
+currency_data = currency["rates"]
+#BASE = EUR
+user = input("Enter a currency : ")
+for i,j in currency_data.items():
+    if i == user:
+        print("Current Value : ", j)
