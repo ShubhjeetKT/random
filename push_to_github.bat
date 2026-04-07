1df7230cb6271d0daf66b4ad3c1beb5f00fe3df3
ShubhjeetKT <shubhjeetkt@users.noreply.github.com>
2026-04-07 12:00:07 +0530

    Use portable Git in push scripts

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,11 +1,13 @@
 @echo off
 cd /d "%~dp0"
 
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote remove origin >nul 2>nul
-git remote add origin https://github.com/ShubhjeetKT/random.git
-git push -u origin main
+set GIT_EXE=C:\Users\tiwari.sk\tools\mingit\cmd\git.exe
+
+"%GIT_EXE%" init
+"%GIT_EXE%" add .
+"%GIT_EXE%" commit -m "Initial commit"
+"%GIT_EXE%" branch -M main
+"%GIT_EXE%" remote remove origin >nul 2>nul
+"%GIT_EXE%" remote add origin https://github.com/ShubhjeetKT/random.git
+"%GIT_EXE%" push -u origin main
 pause
--- push_to_github.ps1
+++ push_to_github.ps1
@@ -1,9 +1,11 @@
 Set-Location "$PSScriptRoot"
 
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote remove origin 2>$null
-git remote add origin https://github.com/ShubhjeetKT/random.git
-git push -u origin main
+$git = "C:\Users\tiwari.sk\tools\mingit\cmd\git.exe"
+
+& $git init
+& $git add .
+& $git commit -m "Initial commit"
+& $git branch -M main
+& $git remote remove origin 2>$null
+& $git remote add origin https://github.com/ShubhjeetKT/random.git
+& $git push -u origin main
